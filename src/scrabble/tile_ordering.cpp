47d6814329da9086846408afb3e3e123b3fda76e
John O'Laughlin <olaughlin@gmail.com>
2023-09-01 16:44:25 -0700

    random tile orderings and test

--- src/scrabble/tile_ordering.cpp
+++ src/scrabble/tile_ordering.cpp
@@ -1,5 +1,8 @@
 #include "src/scrabble/tile_ordering.h"
 
+#include <absl/random/random.h> 
+
+#include "src/scrabble/bag.h"
 #include "src/scrabble/data_manager.h"
 
 namespace {
@@ -34,7 +37,29 @@ TileOrdering::TileOrdering(const q2::proto::TileOrdering& proto,
     : letters_(GetLettersFromProto(proto, tiles)),
       exchange_insertion_dividends_(GetDividendsFromProto(proto)) {}
 
-TileOrdering TileOrdering::Adjust(const std::vector<Letter>& used_letters) const {
+namespace {
+std::vector<Letter> Shuffle(const Tiles& tiles, absl::BitGenRef gen) {
+  Bag bag(tiles);
+  bag.Shuffle(gen);
+  return bag.Letters();
+}
+
+std::vector<uint16_t> RandomDividends(absl::BitGenRef gen, int size) {
+  std::vector<uint16_t> dividends;
+  dividends.reserve(size);
+  for (int i = 0; i < size; ++i) {
+    dividends.push_back(absl::Uniform<uint16_t>(gen, 0, 65535));
+  }
+  return dividends;
+}
+}  // namespace
+TileOrdering::TileOrdering(const Tiles& tiles, absl::BitGenRef gen,
+                           int num_dividends)
+    : letters_(Shuffle(tiles, gen)),
+      exchange_insertion_dividends_(RandomDividends(gen, num_dividends)) {}
+
+TileOrdering TileOrdering::Adjust(
+    const std::vector<Letter>& used_letters) const {
   std::vector<Letter> letters = letters_;
   // Remove used_letters from letters ordering, taking away letters at the back
   // of the list first.
--- src/scrabble/tile_ordering.h
+++ src/scrabble/tile_ordering.h
@@ -1,8 +1,10 @@
 #ifndef SRC_SCRABBLE_TILE_ORDERING_H
 #define SRC_SCRABBLE_TILE_ORDERING_H
 
+#include <cstdint>
 #include <vector>
 
+#include "absl/random/bit_gen_ref.h"
 #include "src/scrabble/computer_players.pb.h"
 #include "src/scrabble/strings.h"
 #include "src/scrabble/tiles.h"
@@ -14,6 +16,7 @@ class TileOrdering {
                const std::vector<uint16_t>& exchange_insertion_dividends)
       : letters_(letters),
         exchange_insertion_dividends_(exchange_insertion_dividends) {}
+  TileOrdering(const Tiles& tiles, absl::BitGenRef gen, int num_dividends);
 
   TileOrdering Adjust(const std::vector<Letter>& used_letters) const;
 
